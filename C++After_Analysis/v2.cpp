714841243566bd1d65aad5eff66e58dfe445f787
LongKey.G <hanfeng0409@yeah.net>
2018-10-28 10:08:32 +0800

    code and data

---
+++ C++After_Analysis/v2.cpp
@@ -0,0 +1,123 @@
+// GooglePractice.cpp: 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+
+	int n = 0;
+	int m = 0;
+	unsigned case_num = 1;
+	int some_case_num = 0;
+	vector<int> case_serial_num;
+	vector<vector<long long int> > case_data;
+	vector<long long int> tmp_vector;
+	int tmp;
+	std::string line;
+	std::ofstream outfile("c:\\users\\gaozb\\desktop\\a-large-practice.out");
+	std::ifstream t("c:\\users\\gaozb\\desktop\\a-large-practice.in"); //读文件ifstream,写文件ofstream，可读可写fstream
+	std::stringstream buffer;
+	//buffer << t.rdbuf();
+	//std::string s = buffer.str();
+
+
+	if (t)
+	{
+		while (!t.eof())//(getline(t,line))
+		{
+			if (n == 0)
+			{
+				t >> case_num;
+			}
+			else
+			{
+				if (m == 0)
+				{
+					t >> some_case_num;
+					case_serial_num.push_back(some_case_num);
+					m++;
+				}
+				else if (m <= some_case_num)
+				{
+					t >> tmp;
+					tmp_vector.push_back(tmp);
+					m++;
+				}
+				else
+				{
+					m = 1;
+					t >> some_case_num;
+					case_data.push_back(tmp_vector);
+					tmp_vector.clear();
+					case_serial_num.push_back(some_case_num);
+				}
+			}
+			n++;
+		}
+	}
+
+
+	case_data.push_back(tmp_vector);
+	tmp_vector.clear();
+	case_serial_num.push_back(some_case_num);
+	t.close();
+
+	long long int z = 0;
+	vector<vector<long long int>>::iterator it = case_data.begin();
+	for (unsigned int i = 0; i < case_num; i++, ++it)
+	{
+		long long int ans = 0;
+		tmp_vector = *it;
+		sort(tmp_vector.begin(), tmp_vector.begin()+ tmp_vector.size());
+
+		std::unordered_map<long long int, long long int> map;
+
+		for ( int a = tmp_vector.size()-1; a >= 0; a--)
+		{
+			for ( int b = a - 1; b >= 0; b--)
+			{
+				z = tmp_vector[a] * tmp_vector[b];
+				if (z != 0)
+				{
+					if (map.find(z) != map.end())
+					{
+						ans = ans + map[int(z)];
+					}
+				}
+				else
+				{
+					ans = ans + b;
+				}
+			}
+
+			if (map.find(tmp_vector[a]) != map.end())
+			{
+				map[int(tmp_vector[a])] = map[int(tmp_vector[a])] + 1;
+			}					
+			else
+			{
+				map[int(tmp_vector[a])] = 1;
+			}
+						
+			
+		}
+		outfile << "Case #" << i + 1 << ": " << ans << "\n";
+		std::cout << ans << endl;
+		tmp_vector.clear();
+	}
+
+	outfile.close();
+
+	return 0;
+}
+
---
+++ Python/After.py
@@ -0,0 +1,39 @@
+cases_num = 0
+num_of_one_case = []
+n=0
+case_data = []
+with open(r'C:\Users\Gaozb\Desktop\A-small-practice.in', 'r') as f:
+    data = f.readlines()  # txt中所有字符串读入data
+    for line in data:
+        linedata = line.split()
+        numbers_float = list(map(float, linedata))
+        if n==0:
+            cases_num = int(numbers_float[0])
+        if n>0 and n%2==0:
+            case_data.append(numbers_float)
+        if n > 0 and n % 2 != 0:
+            num_of_one_case.append(int(numbers_float[0]))
+        n += 1
+
+with open(r'C:\Users\Gaozb\Desktop\A-small-practice.out', 'w') as f:
+    for sample in range(len(case_data)):
+        a,b = 0,1
+        ans = 0
+        arr_sorted = sorted(case_data[sample])
+        d = {}
+        for a in range(num_of_one_case[sample]-1,-1,-1):
+            for b in range(a - 1, -1, -1):
+                z = arr_sorted[a] * arr_sorted[b]
+                if z!= 0:
+                    if z in d.keys():
+                        ans = ans + d[int(z)]
+                else:
+                    ans = ans + b
+            if arr_sorted[a] in d.keys():
+                d[int(arr_sorted[a])] = d[int(arr_sorted[a])]+1
+            else:
+                d[int(arr_sorted[a])]=1
+
+        print(sample,ans)
+        f.write('Case #' + str(int(sample+1))+ ': '+str(int(ans))+'\n')
+
